c6ec7ee97409e373bf4cf3e2e7e8297b4023293d
Benjamin <kogbeb19@htl-kaindorf.at>
2023-01-25 09:31:42 +0100

    created Backend-Project

---
+++ backend/src/main/java/com/medicalview/backend/backend/MedicalViewApplication.java
@@ -0,0 +1,9 @@
+package com.medicalview.backend.backend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MedicalViewApplication extends Application {
+
+}
