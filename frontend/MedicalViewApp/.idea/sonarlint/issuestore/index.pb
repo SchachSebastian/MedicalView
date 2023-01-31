6354b7c73f920c9b190ade194a39645853701812
Sebastian Schachner <sacseb19@htl-kaindorf.at>
2023-01-31 10:28:08 +0100

    medicalmap basic layout implemented

---
+++ frontend/MedicalViewApp/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+D
+my-app/tsconfig.json,6\4\640f89aade6b10e9cf257ca3b7d8a5b6290a78a3
+U
+%.idea/copyright/profiles_settings.xml,8\c\8c00054dc44a9aac96957ae70e12d95773e087f7
+A
+my-app/.gitignore,c\8\c8ab888dbbe8636ccd9f8daa3b3183e033677d52
+K
+my-app/public/manifest.json,8\5\85fe4fee5864fcd0abdf05b5d0657f73e43a59d8
+H
+my-app/public/robots.txt,7\7\77180da6a3a5113133f5558cc76f956985af805a
+@
+my-app/README.md,c\4\c46e4e9d189721e177f72e4ced4b3df7c28f57ee
--- frontend/MedicalViewApp/my-app/src/MedicalMap.tsx
+++ frontend/MedicalViewApp/my-app/src/MedicalMap.tsx
@@ -1,5 +1,7 @@
 import {useState} from 'react';
-import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet';
+import {MapContainer, TileLayer, useMap} from 'react-leaflet';
+import './MedicalMap.css';
+import 'leaflet/dist/leaflet.css';
 
 interface MedicalMapProps {
     coords: {
@@ -9,31 +11,24 @@ interface MedicalMapProps {
     display_name: string;
 }
 
-function MedicalMap(props: MedicalMapProps) {
+function ChangeView() {
+    const map = useMap();
+    map.setView([1, 2], 13);
+    return null;
+}
+
+export const MedicalMap = (props: MedicalMapProps) => {
     const [markers, setMarkers] = useState([
         {lat: 51.505, lng: -0.09, popup: 'Marker 1'},
         {lat: 51.506, lng: -0.09, popup: 'Marker 2'},
         {lat: 51.507, lng: -0.09, popup: 'Marker 3'},
     ]);
-
-    function MapView() {
-        let map = useMap();
-        map.setView(props.coords, map.getZoom());
-        //Sets geographical center and zoom for the view of the map
-        return null;
-    }
-
+    const coords = [props.coords.latitude, props.coords.longitude];
     return (
-        <MapContainer bounds={props.coords} boundsOptions={13}>
+        <MapContainer className={'map'}>
+            <ChangeView/>
             <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {markers.map((marker, index) => (
-                <Marker key={index} position={[marker.lat, marker.lng]}>
-                    <Popup>{marker.popup}</Popup>
-                </Marker>
-            ))}
-        </MapContainer>
-    );
+        </MapContainer>);
 }
